40e89441cf51530030e15249d2f8f0f7c202ed34
Hugh Mann <157652589+kp53562@users.noreply.github.com>
2026-01-27 16:59:55 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 // ideas: store filesystem information in a downloadable file, like a virtual drive. make files creatable and editable, maybe even uploadable. 
-var filenumber = 0
-function open(){
+var filenumber = 0;
+function opendir(){
+  document.body.appendChild(document.createElement("br"));
   var file = document.createElement("button");
   file.innerText = "file"+String(filenumber);
   filenumber+=1;
-  file.setAttribute("onclick","open()");
+  file.setAttribute("onclick","opendir()");
   document.body.appendChild(file);
 }
 
 const rootdir = document.createElement("button");
 rootdir.innerText = ":::filesystem:::"
-rootdir.setAttribute("onclick","open()");
+rootdir.setAttribute("onclick","opendir()");
 document.body.appendChild(rootdir);
