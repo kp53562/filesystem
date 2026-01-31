383d3dcddf09f34ba0f2014cdf1686109b0a7575
Hugh Mann <157652589+kp53562@users.noreply.github.com>
2026-01-30 17:59:13 -0800

    add debug to the right(hopefully) places

--- main.js
+++ main.js
@@ -52,8 +52,6 @@ function searchDirs(dir,id){
 }
 
 function getObjwId(id){
-  alert(String(dirvalues.size))
-  alert(String(dirvalues.keys()))
   if (dirvalues.size < 1)
     return null;
   else{
@@ -81,7 +79,7 @@ function hideObjsWithinDir(id){
       document.getElementById(id).setAttribute("opened","closed");
       for (const elem of objection.getLeft().values()) {
         if (elem instanceof Pair) {
-          alert(String(elem.getRight));
+          alert(String(elem.getRight()));
           if (elem.getLeft() instanceof Map)
             hideObjsWithinDir(elem.getRight());
           document.getElementById(elem.getRight()).remove();
@@ -103,6 +101,7 @@ function showObjsWithinDir(id){
   var objection = getObjwId(id);
   if (objection == -1)
     objection = new Pair(dirvalues,-1);
+  alert(objection);
   if (objection != null) {
     if (objection instanceof Pair) {
       if (objection.getLeft() instanceof Map) {
@@ -116,11 +115,12 @@ function showObjsWithinDir(id){
                     break;
                   Anarchic = Anarchic.slice(1);
                 }
+                alert(Anarchic);
                 if (strIsNum(String(parseInt(Anarchic))))
                     indentpos = parseInt(Anarchic)+15;
                 document.body.appendChild(document.createElement("br"));
                 var content = document.createElement("button");
-                content.innerText = objection.getLeft().keys()[x];
+                content.innerText = objection.getLeft().keys().toArray()[x];
                 content.setAttribute("style","margin-left: "+String(indentpos)+"px;");
                 if (elem.getLeft() instanceof Map)
                   content.setAttribute("onclick","opendir("+String(elem.getRight())+")");
@@ -143,14 +143,11 @@ function newfile(id){
     dirtoinsertin = getObjwId(id);
   if (dirtoinsertin == 0) {
     dirvalues.set("File " + String(dirnumber), new Pair(" ",dirnumber));
-    alert("this is a test");
   }
   else
     dirtoinsertin.set("File " + String(dirnumber), new Pair(" ",dirnumber));
-  alert("a")
   hideObjsWithinDir(id);
   showObjsWithinDir(id);
-  alert("b")
   dirnumber+=1;
 }
 
