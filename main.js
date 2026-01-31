0fc6f7b2b5abd5bbc48184b54ccf0e20ce29f6da
Hugh Mann <157652589+kp53562@users.noreply.github.com>
2026-01-30 18:02:23 -0800

    more debuggggg

--- main.js
+++ main.js
@@ -101,7 +101,7 @@ function showObjsWithinDir(id){
   var objection = getObjwId(id);
   if (objection == -1)
     objection = new Pair(dirvalues,-1);
-  alert(objection);
+  alert("objection: "+objection.toString());
   if (objection != null) {
     if (objection instanceof Pair) {
       if (objection.getLeft() instanceof Map) {
